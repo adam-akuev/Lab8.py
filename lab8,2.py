0582b5975ad127d937e77acc2426c2e35907349b
adams095 <akuev2005@mail.ru>
2023-12-13 00:49:05 +0300

    Task2

---
+++ lab8,2.py
@@ -0,0 +1,18 @@
+def SortDec3(A, B, C):
+    Mas = [A, B, C]
+    for i in range(1, 3):
+        if Mas[1] < Mas[0]:
+            Mas[0], Mas[1] = Mas[1], Mas[0]
+        if Mas[1] > Mas[2]:
+            Mas[1], Mas[2] = Mas[2], Mas[1]
+    return Mas
+
+try:
+    for i in range(2):
+        A1 = float(input('Введите значение A:'))
+        B1 = float(input('Введите значение B:'))
+        C1 = float(input('Введите значение C:'))
+        CCC = SortDec3(A=A1, B=B1, C=C1)
+        print(CCC)
+except ValueError:
+    print('Введите корректные данные!!!')
