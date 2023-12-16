c20f152601e33c19d863639c3503b71753e8bb3a
adams095 <akuev2005@mail.ru>
2023-12-17 00:29:36 +0300

    Task2 and Task3

---
+++ lab8,3.py
@@ -0,0 +1,22 @@
+import random
+
+def Max(mas, leftMas, rightMas):
+    if leftMas == rightMas:
+        return mas[leftMas]
+
+    mid = (leftMas + rightMas) // 2
+
+    max_left = Max(mas, leftMas, mid)
+    max_right = Max(mas, mid + 1, rightMas)
+    if max_right > max_left:
+        return max_right
+    else:
+        return max_left
+
+mas = []
+count = int(input("Введите количество рандомных чисел: "))
+for i in range(1, count+1):
+    mas.append(random.randint(-100, 100))
+n = len(mas)
+print(mas)
+print(f'Max: {Max(mas, 0, n - 1)}')
